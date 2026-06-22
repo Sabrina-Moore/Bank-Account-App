4066050e6f69f2f20e7ea7df8941113ff0284c73
Sabrina Moore <mooresabrina7@gmail.com>
2026-06-21 21:19:26 -0700

    Fixed variable types
    Switched variable types from int to string, and changed passwordLogin() to return a pointer for consistency.

--- userInput.cpp
+++ userInput.cpp
@@ -1,38 +1,40 @@
 //acquires password and command request
 #include <iostream>
 #include <string>
+#include <vector>
 #include "BankAccountClass.h"
 using namespace std;
 
-void passwordLogin(vector<BankAccount> accounts) {
-	
-	int username;
-	int password; //to cin password
-	cout << "Welcome to the bank. Enter username to access your account: " << endl;
-	cin >> username;
+BankAccount* passwordLogin(vector<BankAccount>& accounts) {
 
-	//check for username
-	BankAccount* matchedAccount = nullptr;
+    string username;
+    string password; //to cin password
+    cout << "Welcome to the bank. Enter username to access your account: " << endl;
+    cin >> username;
+
+    //check for username
+    BankAccount* matchedAccount = nullptr;
     for (int i = 0; i < accounts.size(); i++) {
         if (accounts[i].getAccountNumber() == username) {
             matchedAccount = &accounts[i];
             break;
         }
     }
-	 if (matchedAccount == nullptr) {
+    if (matchedAccount == nullptr) {
         cout << "No account found with that username." << endl;
         return nullptr;
     }
 
-	//check for password
-	cin.ignore();
-	cout << "Enter password: " << endl;
-	cin >> password;
+    //check for password
+    cin.ignore();
+    cout << "Enter password: " << endl;
+    cin >> password;
 
-	if (matchedAccount->verifyPassword(password)) {
+    if (matchedAccount->verifyPassword(password)) {
         cout << "Access granted." << endl;
         return matchedAccount;
-    } else {
+    }
+    else {
         cout << "Incorrect password. Access denied." << endl;
         return nullptr;
     }
